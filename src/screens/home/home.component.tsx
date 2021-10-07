4dc62cea3ff77f3d66a3b426dcda9a45d2254de1
Paulo Luan <pauloluan.inova@gmail.com>
2021-10-07 05:10:35 -0300

    add: little test in order to test the constants mapping and the remote cdn image

--- src/screens/home/home.component.tsx
+++ src/screens/home/home.component.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import * as S from './home.styles'
+import { testament } from '@constants/Images'
 
 export interface HomeProps {
   testId?: string
@@ -10,6 +11,14 @@ export const Home = ({ testId = 'component-home-id', ...props }: HomeProps) => {
     <>
       <S.Wrapper testID={testId} {...props}>
         {/* <S.Text>sdfa </S.Text> */}
+        <S.Image
+          source={{
+            uri: testament.new.eph
+          }}
+        />
+        <S.Card></S.Card>
+        <S.Card />
+        <S.Card />
       </S.Wrapper>
     </>
   )
--- src/screens/home/home.styles.ts
+++ src/screens/home/home.styles.ts
@@ -5,6 +5,19 @@ import styled, { css } from 'styled-components/native'
 export const Wrapper = styled.View`
   ${() => css`
     flex: 1;
-    background-color: blue;
+    background-color: ${({ theme }) => theme.colors.background};
   `}
+`
+
+export const Card = styled.View`
+  width: 150px;
+  height: 250px;
+  margin: 10px;
+  border-radius: 6px;
+  background-color: red;
+`
+
+export const Image = styled.Image`
+  height: 56px;
+  width: 56px;
 `
