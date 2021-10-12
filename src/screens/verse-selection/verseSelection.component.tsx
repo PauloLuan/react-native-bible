1073eb38f8061696943847a4188ee5c0c90d03d9
Paulo Luan <pauloluan.inova@gmail.com>
2021-10-12 05:04:56 -0300

    📦 new: added initial version of list selection screen

--- src/screens/verse-selection/verseSelection.component.tsx
+++ src/screens/verse-selection/verseSelection.component.tsx
@@ -1,13 +1,40 @@
 import React from 'react'
 import * as S from './verseSelection.styles'
 
+interface ListItem {
+  id: number
+  teste: number
+}
+
+const data = [
+  { id: 1, teste: 1 },
+  { id: 2, teste: 2 },
+  { id: 3, teste: 3 }
+]
+
 export interface VerseSelectionProps {
   testId?: string
+}
+
+const renderItem = ({ item, index }: { item: ListItem; index: number }) => {
+  return (
+    <S.Item>
+      <S.ItemText>{item.id}</S.ItemText>
+    </S.Item>
+  )
 }
 
 export const VerseSelection = ({
   testId = 'component-verse-selection-id',
   ...props
 }: VerseSelectionProps) => {
-  return <S.Wrapper testID={testId} {...props}></S.Wrapper>
+  return (
+    <S.Wrapper testID={testId} {...props}>
+      <S.ListItems<React.ElementType>
+        data={data}
+        keyExtractor={(item: ListItem) => item.id}
+        renderItem={renderItem}
+      ></S.ListItems>
+    </S.Wrapper>
+  )
 }
--- src/screens/verse-selection/verseSelection.styles.ts
+++ src/screens/verse-selection/verseSelection.styles.ts
@@ -1,12 +1,36 @@
-import styled, { css } from 'styled-components/native'
-
-// import { VerseSelectionProps } from '.'
+import { FlatList } from 'react-native'
+import { RFValue } from 'react-native-responsive-fontsize'
+import styled from 'styled-components/native'
 
 export const Wrapper = styled.View`
-  ${() => css`
-    flex: 1;
-    background-color: blue;
-    width: 85px;
-    height: 60px;
-  `}
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
+`
+
+interface ListItem {
+  id: number
+  teste: number
+}
+
+export const ListItems = styled(FlatList as new () => FlatList<ListItem>).attrs(
+  {
+    horizontal: true
+  }
+)`
+  flex: 1;
+`
+
+export const Item = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 36px;
+  height: 36px;
+  margin: ${RFValue(4)}px;
+  background: ${({ theme }) => theme.colors.sandDark};
+`
+
+export const ItemText = styled.Text`
+  font-size: ${RFValue(24)}px;
 `
